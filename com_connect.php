efe2953ee4c4c964fd8ad6869a7a897a58d41054
etc <haut.buis@aliceadsl.fr>
2021-08-01 21:29:36 +0200

    Update com_connect.php

--- com_connect.php
+++ com_connect.php
@@ -1669,11 +1669,9 @@ function com_connect_group_validate()
  */
 function com_connect_lAtts($pairs, $atts)
 {
-    // Prevent these global attributes from interfering.
+    // Prevent global attributes from interfering.
     // See https://github.com/textpattern/textpattern/pull/893
-    lAtts(array(
-        'label' => '',
-    ), $atts, false);
+    lAtts($pairs, $atts, false);
 
     foreach (array('button', 'copysender', 'checked', 'required', 'send_article', 'show_input', 'show_error') as $key) {
         if (isset($atts[$key])) {
